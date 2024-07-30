b31e76dd226d438026af19acd0d9d6428d3e4d7c
Yosuke <ysk0805.bv@gmail.com>
2024-07-30 17:25:32 +0100

    changed CV

--- pages/2_📝_Resume.py
+++ pages/2_📝_Resume.py
@@ -20,7 +20,7 @@ st.title("📝 Resume")
 
 st.write("[Click here if it's blocked by your browser](https://drive.google.com/file/d/1o0O3MPEXFapzE1uAZBfE2qw5gduXZHf4/view?usp=sharing)")
 
-with open("images/CV.pdf","rb") as f:
+with open("images/Yosuke_Kawazoe_CV.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
