1c178dced4c061e5a5bca505dd108291c9d8fb94
Yosuke Kawazoe <ysk0805.bv@gmail.com>
2024-09-02 12:18:23 +0100

    modified

--- pages/2_📝_Resume.py
+++ pages/2_📝_Resume.py
@@ -20,7 +20,7 @@ st.title("📝 Resume")
 
 st.write("[Click here if it's blocked by your browser](https://drive.google.com/file/d/1PvI-MCw2TtCEAU7d7S_QZqSFhaf15GkB/view?usp=sharing)")
 
-with open("images/Daniel_Kawazoe_CV.pdf","rb") as f:
+with open("images/Yosuke(Daniel)_Kawazoe_CV.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
