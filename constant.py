5d3ed5e0174ab4c423fc64664938282b99c1a161
Yosuke Kawazoe <ysk0805.bv@gmail.com>
2024-09-03 12:44:06 +0100

    change constan.py

--- constant.py
+++ constant.py
@@ -3,7 +3,7 @@ info = {
    "Name": "Yosuke",
    "Full_Name": "Yosuke Kawazoe",
    "Intro": "A Data Scientist/Machine learning engineer and AI Enthusiast",
-   "About":"Hey there, I'm Yosuke! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. I have been wanting to live in an English speaking country because that is my dream. Acheving my dream, now I have a Youth Mobility Scheme Visa from 07/2024 and would like to switch to work visa. Wanna know more about my backgroud? Check out my LinkedIn and reach me out!: https://www.linkedin.com/in/yosuke-kawazoe-8782b218b/",
+   "About":"Hey there, I'm Yosuke! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. I have been wanting to live in an English speaking country because that is my dream. To acheve my dream, I came to the UK and am constantly learning techs as well as improving my English skills. Wanna know more about my backgroud? Check out my LinkedIn and reach me out!: https://www.linkedin.com/in/yosuke-kawazoe-8782b218b/",
    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
    "Medium":"https://medium.com/@vicky-note/about",
    "City":"London, United Kingdam",
