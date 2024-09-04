d469c7b60ff4b2aadc4356793b3996e959e44f79
Yosuke Kawazoe <ysk0805.bv@gmail.com>
2024-09-04 15:51:33 +0100

    modified hobby page

--- pages/3_💻_App.py
+++ pages/3_💻_App.py
@@ -20,18 +20,19 @@ st.title("💻 My applications")
 st.markdown("These are the application that I built by myself in my spare time. Let me know what you think and feel free to request any functions!!")
 
 # summarizing meeting minutes from audio file
-st.subheader("Summarizing meeting minutes from audio file")
+st.header("Summarizing meeting minutes from audio file")
 description = """
 Have you ever thought that you wanted to automate creating meeting minutes especially when you attend international meetings where people speak differenct languages?
 Well, this app is for you!!
 All you have to do for taking advantage this app is following two things.
 1. Record a meeting.
 2. Generate gemini API key and click on the button. You can generate your gemini API key [here](https://aistudio.google.com/app/apikey).
+
 When you open the app, drop down audio file that you record and select the original language that is spoken in meeting and the target language that you want to summarize in and click on the button.
 This way, you won't waste your precious time and it is completely free!!
 """
 st.markdown(description)
-st.markdown("[Click here to visit my app for summarizing meeting meniuts from audio file!!](https://summarizemeetingminutesapp-bzha9c2qlxsveh46xmeebm.streamlit.app/)")
+st.markdown("[Click [here](https://summarizemeetingminutesapp-bzha9c2qlxsveh46xmeebm.streamlit.app/) to visit my app for summarizing meeting meniuts from audio file!!")
 
 
 
