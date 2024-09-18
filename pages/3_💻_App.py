e7faf45cb86ec3bf898e45447600afbce79a056d
Yosuke Kawazoe <ysk0805.bv@gmail.com>
2024-09-18 14:45:09 +0100

    update

--- pages/3_💻_App.py
+++ pages/3_💻_App.py
@@ -19,10 +19,28 @@ st.title("💻 My applications")
 
 st.markdown("**These are the application that I built by myself in my spare time. Let me know what you think and feel free to request any functions!!**")
 
-# summarizing meeting minutes from audio file
-st.header("Summarizing meeting minutes from audio file")
+# recommending movies
+st.header("🎥 Reccomending movies")
 st.subheader("Discription")
-description = """
+description_movies = """
+Have you ever experienced binging watch movies and do not know what to watch next but too dull to search movies??
+Here is the app for you!
+All you have to do is choose the category and movies that you like and click on the button and show top 10 recommended movies.
+I am afraid that this app is not personalized to your taste. If you want to personalized, let me know I will try to create personalized recommendation;)
+Check it out!
+"""
+st.markdown(description_movies)
+st.markdown("Click below button to visit my app for recommending movies!!")
+url_movies = "https://moviesrecommenderapp-by-yosuke.streamlit.app/"
+# Create a button with HTML
+st.markdown(f"""
+    <a href="{url_movies}" type="url_app" target="_blank" style="display: inline-block; padding: 10px 20px; font-size: 16px; font-weight: bold; color: white; background-color: #FFFF00; border-radius: 5px; text-decoration: none;">Try app</a>
+""", unsafe_allow_html=True)
+
+# summarizing meeting minutes from audio file
+st.header("📋 Summarizing meeting minutes from audio file")
+st.subheader("Discription")
+description_summary = """
 Have you ever thought that you wanted to automate creating meeting minutes especially when you attend international meetings where people speak differenct languages?
 Well, this app is for you!!  
 All you have to do for taking advantage this app is following two things.
@@ -32,12 +50,12 @@ All you have to do for taking advantage this app is following two things.
 When you open the app, drop down audio file that you record and select the original language that is spoken in meeting and the target language that you want to summarize in and click on the button.
 This way, you won't waste your precious time and it is completely free!!
 """
-st.markdown(description)
+st.markdown(description_summary)
 st.markdown("Click below button to visit my app for summarizing meeting meniuts from audio file!!")
-url = "https://summarizemeetingminutesapp-bzha9c2qlxsveh46xmeebm.streamlit.app/"
+url_summary = "https://summarizemeetingminutesapp-bzha9c2qlxsveh46xmeebm.streamlit.app/"
 # Create a button with HTML
 st.markdown(f"""
-    <a href="{url}" type="url_app" target="_blank" style="display: inline-block; padding: 10px 20px; font-size: 16px; font-weight: bold; color: white; background-color: #FFFF00; border-radius: 5px; text-decoration: none;">Try app</a>
+    <a href="{url_summary}" type="url_app" target="_blank" style="display: inline-block; padding: 10px 20px; font-size: 16px; font-weight: bold; color: white; background-color: #FFFF00; border-radius: 5px; text-decoration: none;">Try app</a>
 """, unsafe_allow_html=True)
 
 
